5406801739a1dd1d4eb1c0730da8670dc463a534
Cwei0 <109186036+Cwei0@users.noreply.github.com>
2023-09-01 22:48:09 +0100

    feat: gathered all assets and devloped structure for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Footer, Hero, Meals, Navbar, Review } from "./layouts"
 const App = () => {
   return (
     <div className="app">
-      <Navbar/>
-      <Hero/>
-      <Meals/>
-      <Review/>
-      <Footer/>
+      <div className="main">
+        <Navbar/>
+        <Hero/>
+        <Meals/>
+        <Review/>
+        <Footer/>
+      </div>
     </div>
   )
 }
--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Logo } from "../../components";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav>
+        {/* Left section */}
+        <Logo />
+        {/* Midddle section */}
+        <div>Menu bar</div>
+        {/* Right section */}
+        <div>Search, basket and User login</div>
+      </nav>
+    </>
+  );
+};
